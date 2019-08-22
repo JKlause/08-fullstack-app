64347cf8b431fc5d756ca71a055da804511eed0b
Joe Klause <joeklause@gmail.com>
2019-08-22 09:43:37 -0700

    Filled out trumpet data

---
+++ data/trpt-data.js
@@ -0,0 +1,162 @@
+module.exports = [
+    {
+        lastName: 'Work',
+        firstName: 'Jeff',
+        age: 45,
+        mainType: 'Orchestral',
+        secondType: 'Soloist',
+        mainEnsemble: 'Oregon Symphony',
+        secondEnsemble:'n/a',
+        isCollegeTeacher: false,
+    },
+    {
+        lastName: 'Viens',
+        firstName: 'Sarah',
+        age: 38,
+        mainType: 'Orchestral',
+        secondType: 'Free-lance',
+        mainEnsemble: 'Pacific Northwest Ballet',
+        secondEnsemble: 'Eugene Symphony',
+        isCollegeTeacher: true
+    },
+    {
+        lastName: 'Bamonte',
+        firstName: 'Dave',
+        age: 55,
+        mainType: 'Orchestral',
+        secondType: 'n/a',
+        mainEnsemble: 'Oregon Symphony',
+        secondEnsemble: 'n/a',
+        isCollegeTeacher: true
+    },
+    {
+        lastName: 'Reneau',
+        firstName: 'Doug',
+        age: 40,
+        mainType: 'Orchestral',
+        secondType: 'n/a',
+        mainEnsemble: 'Oregon Symphony',
+        secondEnsemble: 'n/a',
+        isCollegeTeacher: false
+    },
+    {
+        lastName: 'Rutherford',
+        firstName:'Bob',
+        age: 65,
+        mainType: 'Orchestral',
+        secondType: 'Free-lance',
+        mainEnsemble: 'Oregon Ballet Theatre',
+        secondEnsemble: 'Portland Opera',
+        isCollegeTeacher: false
+    },
+    {
+        lastName: 'Gibson',
+        firstName: 'Craig',
+        age: 65,
+        mainType: 'Free-lance',
+        secondType: 'Orchestral',
+        mainEnsemble: 'n/a',
+        secondEnsemble: 'n/a',
+        isCollegeTeacher: true
+    },
+    {
+        lastName: 'Thane',
+        firstName: 'Logan',
+        age: 32,
+        mainType: 'Free-lance',
+        secondType: 'Orchestral',
+        mainEnsemble: 'Eugene Concert Orchestra',
+        secondEnsemble: 'Rose City Brass Quintet',
+        isCollegeTeacher: false
+    },
+    {
+        lastName: 'Klause',
+        firstName: 'Joe',
+        age: 32,
+        mainType: 'Free-lance',
+        secondType: 'Orchestral',
+        mainEnsemble: 'Eugene Symphony',
+        secondEnsemble: 'Rose City Brass Quintet',
+        isCollegeTeacher: true
+    },
+    {
+        lastName: 'Smock',
+        firstName: 'James',
+        age: 38,
+        mainType: 'Free-lance',
+        secondType: 'Orchestral',
+        mainEnsemble: 'Portland Columbia Symphony',
+        secondEnsemble: 'East Oregon Symphony',
+        isCollegeTeacher: false
+    },
+    {
+        lastName: 'Conrow',
+        firstName: 'Steve',
+        age: 55,
+        mainType: 'Free-lance',
+        secondType: 'Orchestral',
+        mainEnsemble: 'Oregon Symphony',
+        secondEnsemble: 'n/a',
+        isCollegeTeacher: true
+    },
+    {
+        lastName: 'Dover',
+        firstName: 'John',
+        age: 40,
+        mainType: 'Free-lance',
+        secondType: 'Commercial',
+        mainEnsemble: 'Portlandia Brass Ensemble',
+        secondEnsemble: 'n/a',
+        isCollegeTeacher: false
+    },
+    {
+        lastName: 'Barbur',
+        firstName: 'Tom',
+        age: 40,
+        mainType: 'Commercial',
+        secondType: 'Free-lance',
+        mainEnsemble: 'Dirty Revival',
+        secondEnsemble: 'n/a',
+        isCollegeTeacher: false
+    },
+    {
+        lastName: 'Titterington',
+        firstName: 'Dick',
+        age: 55,
+        mainType: 'Free-lance',
+        secondType: 'Commercial',
+        mainEnsemble: 'n/a',
+        secondEnsemble: 'n/a',
+        isCollegeTeacher: false
+    },
+    {
+        lastName: 'Dunn',
+        firstName: 'Bruce',
+        age: 45,
+        mainType: 'Free-lance',
+        secondType: 'Orchestral',
+        mainEnsemble: 'Vancouver Symphony',
+        secondEnsemble: 'n/a',
+        isCollegeTeacher: false
+    },
+    {
+        lastName: 'Mazzio',
+        firstName: 'Paul',
+        age: 55,
+        mainType: 'Free-lance',
+        secondType: 'Commercial',
+        mainEnsemble: 'n/a',
+        secondEnsemble: 'n/a',
+        isCollegeTeacher: false
+    },
+    {
+        lastName: 'Butler',
+        firstName: 'Charlie',
+        age: 55,
+        mainType: 'Free-lance',
+        secondType: 'Orchestral',
+        mainEnsemble: 'Oregon Symphony',
+        secondEnsemble: 'n/a',
+        isCollegeTeacher: false
+    }
+]
