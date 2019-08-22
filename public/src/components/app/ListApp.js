72f9cfc8f40cabe0525877cfe16ed27f77015f23
Joe Klause <joeklause@gmail.com>
2019-08-22 13:21:32 -0700

    rendered trpt list

--- public/src/components/app/ListApp.js
+++ public/src/components/app/ListApp.js
@@ -1,5 +1,5 @@
 import Component from '../Component.js';
-import Header from '.Header.js';
+import Header from './Header.js';
 
 class ListApp extends Component {
     onRender(dom) {
@@ -7,7 +7,30 @@ class ListApp extends Component {
         dom.prepend(header.renderDOM());
     }
     renderHTML() {
-        
+        return /*html*/`
+        <main>
+            <div id="trumpet-table-main">
+                <h2>Portland Trumpet Player Database</h2>
+                <div>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Last Name</th>
+                                <th>First Name</th>
+                                <th>Age</th>
+                                <th>Main Type</th>
+                                <th>Secondary Type</th>
+                                <th>Main Ensemble</th>
+                                <th>Second Ensemble</th>
+                                <th>Is College Teacher?</th>
+                            </tr>
+                        </thead>
+                        <tbody id="player-table-body"></tbody>
+                    </table>
+                </div>
+            </div>
+        </main>
+    `;
     }
 }
 export default ListApp;
